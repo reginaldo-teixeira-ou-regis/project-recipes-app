70a7d0e15f7b7aadace0436be5657f176114b337
devarturmartins <arturvl1@hotmail.com>
2023-01-31 16:22:50 -0300

    tela de login finalizada - faltando os testes

---
+++ src/components/Profile.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default function Profile() {
+  const storage = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
+
+  const handleClick = ({ target }) => {
+    if (target.value === 'doneRecipes') {
+      history.push('/done-recipes');
+    }
+    if (target.value === 'favoriteRecipes') {
+      history.push('/favorite-recipes');
+    }
+    if (target.value === 'logout') {
+      localStorage.clear();
+      history.push('/');
+    }
+  };
+  return (
+    <div>
+      <h4 data-testid="profile-email">{ storage.email }</h4>
+      <button
+        onClick={ (e) => handleClick(e) }
+        name="buttonProfile"
+        value="doneRecipes"
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        onClick={ (e) => handleClick(e) }
+        name="buttonProfile"
+        value="favoriteRecipes"
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        onClick={ (e) => handleClick(e) }
+        name="buttonProfile"
+        value="logout"
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
+    </div>
+  );
+}
