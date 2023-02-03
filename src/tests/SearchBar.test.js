3ccf90b495ff17d828e0cc5d7c2d8ffe5f6c51f3
Arthur Carvalho <arthurcrsouto@gmail.com>
2023-02-03 15:25:38 -0300

    searchbar com o mesmo problema

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -1,10 +1,18 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 // import userEvent from '@testing-library/user-event';
 import SearchBar from '../components/SearchBar';
 import renderWithRouter from './helpers/renderWithRouter';
 
 describe('Testa o componente SearchBar', () => {
-  test('Verifica se, ao clicar no botão, aparece ', () => {
+  test('Verifica se, ao clicar no botão, aparece a barra de pesquisa', () => {
     renderWithRouter(<SearchBar />);
+    const searchIcon = screen.getByRole('img', {
+      name: /searchicon/i,
+    });
+    const searchInput = screen.getByTestId('search-input');
+
+    userEvent.click(searchIcon);
+    expect(searchInput).toBeInTheDocument();
   });
 });
